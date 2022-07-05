571b9fa3edced137da9de0b4df41e405f753b9ed
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2022-07-05 18:40:31 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,8 +5,10 @@ npm run dist
 
 sudo mkdir /usr/bin/NotElec
 
+# Download Logo
+wget https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png
+
 # Move AppImage to bin folder
 sudo cp "$PWD/dist/NotElec-0.1.0.AppImage" /usr/bin/NotElec/NotElec-0.1.0.AppImage
-sudo cp "$PWD/src/Icon/icon.png" /usr/bin/NotElec/icon.png
+sudo cp "$PWD/src/Icon/Notion_app_logo.png" /usr/bin/NotElec/icon.png
 sudo cp "$PWD/NotElec.desktop" /usr/share/applications/NotElec.desktop
-
