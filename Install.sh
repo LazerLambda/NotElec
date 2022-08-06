18b1aa8910f52e74d796048876819be30686560d
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2022-08-06 04:30:14 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -10,5 +10,5 @@ wget https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png
 
 # Move AppImage to bin folder
 sudo cp "$PWD/dist/NotElec-0.1.0.AppImage" /usr/bin/NotElec/NotElec-0.1.0.AppImage
-sudo cp "$PWD/src/Icon/Notion_app_logo.png" /usr/bin/NotElec/icon.png
+sudo cp "$PWD/Notion_app_logo.png" /usr/bin/NotElec/icon.png
 sudo cp "$PWD/NotElec.desktop" /usr/share/applications/NotElec.desktop
